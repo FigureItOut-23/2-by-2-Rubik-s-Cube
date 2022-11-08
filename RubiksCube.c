a75c5e189155dec16b7d762f613cfcdb62558a06
VishGit1234 <47926842+VishGit1234@users.noreply.github.com>
2022-11-07 19:40:00 -0500

    Added scanning for top face
    Added scanning for top face by rotating cube (need to add bottom face)

--- RubiksCube.c
+++ RubiksCube.c
@@ -28,9 +28,47 @@ for(int face = 0; face < 4; face++)
 	wait1Msec(50);
 	getColorRGB(S2, col_red[face], col_green[face], col_blue[face]);
 	//playSound(soundBeepBeep);
-	rotateFrame(90*(face+1));
+	if(face < 3)
+	{
+		cur_degrees += 90;
+		rotateFrame(cur_degrees);
+	}
 }
+//Flip cube
+motor[motorA] = -10;
+nMotorEncoder[motorA] = 0;
+while(abs(nMotorEncoder[motorA]) < 90)
+{}
+motor[motorA] = 0;
+motor[motorA] = 10;
+while(abs(nMotorEncoder[motorA]) > 0)
+{}
+motor[motorA] = 0;
 
+//Whack Cube
+motor[motorB] = -20;
+wait1Msec(1000);
+motor[motorB] = 0;
+motor[motorB] = 20;
+while(abs(nMotorEncoder[motorB]) > 0)
+{}
+motor[motorB] = 0;
+
+rotateFrame(90);
+
+//Scan Top Face
+wait1Msec(50);
+getColorRGB(S2, col_red[4], col_green[4], col_blue[4]);
+
+//Scane
+
+/*
+for(int face = 4; face < 6; face++)
+{
+	wait1Msec(50);
+	getColorRGB(S2, col_red[face], col_green[face], col_blue[face]);
+}
+*/
 /*
 int whiteSum = 0;
 int whiteFace = 0;
