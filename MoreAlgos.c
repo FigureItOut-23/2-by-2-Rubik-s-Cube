bc2dae2d57cd96ff823d0931b42b2779dde6765e
TheBarrelPhilospher <108647320+TheBarrelPhilospher@users.noreply.github.com>
2022-11-13 19:32:33 -0500

    solveWhite
    solves for the white face

--- MoreAlgos.c
+++ MoreAlgos.c
@@ -286,7 +286,7 @@ void orientFace(int* cube)
 			if(cube[1]==1) //ASSUMING 3 IS GREEN
 			{
 				alg2();
-				U(CW)
+				U(CW);
 				alg3();
 			}
 			else if(cube[2]==1)
@@ -296,7 +296,7 @@ void orientFace(int* cube)
 				returnWhacker();
 				rotate(CW);
 				alg2();
-				U(CW)
+				U(CW);
 				alg3();
 			}
 			else if(cube[3]==1)
@@ -308,7 +308,7 @@ void orientFace(int* cube)
 				rotate(CW);
 				rotate(CW);
 				alg2();
-				U(CW)
+				U(CW);
 				alg3();
 			}
 			else
@@ -318,7 +318,7 @@ void orientFace(int* cube)
 				returnWhacker();
 				rotate(CCW);
 				alg2();
-				U(CW)
+				U(CW);
 				alg3();
 			}
 		}
@@ -367,6 +367,107 @@ void orientFace(int* cube)
 }
 
 
+void solvingWhite(int *cube)
+{
+
+	
+//FACE 1: YELLOW 0-3  // top
+//FACE 2: GREEN 4-7 // left side
+//FACE 3: WHITE 8-11 // bottom
+//FACE 4 : BLUE 12-15 //right side
+//FACE 5: RED 16-19 //front
+//FACE 6: ORANGE 20-23 //back
+	//FINDING FACE WITH MOST WHITE PIECES
+	int whiteAccumulator[6] = {0};
+	//let colour white = 7;
+	int colourWhite = 7;
+	
+	for (int j = 0; j < 24; j++) 
+		{
+			if(j<4)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[0] ++;
+				}
+			}
+			
+			if(j<8 && j >3)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[1] ++;
+				}
+			}
+			
+			if(j<12 && j >7)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[2] ++;
+				}
+			}
+			
+			if(j<16 && j >11)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[3] ++;
+				}
+			}
+			
+			if(j<20 && j > 15)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[4] ++;
+				}
+			}
+			
+			if(j<24 && j >19)
+			{
+				if(cube[j] == colourWhite)
+				{
+				whiteAccumulator[5] ++;
+				}
+			}
+		}
+		
+
+	int mostWhite = -9;
+	int indexFace = -1;
+
+	for (int c = 0; c < 6; c++)
+	{
+		if (whiteAccumulator[c]>mostWhite)
+		{
+			mostWhite = whiteAccumulator[c];
+			indexFace = c;
+		}
+	}
+	
+	//THEREFORE FACE [c] has the most white pieces
+	printf( "Face with most white pieces is %d", indexFace);
+	int tempArray[4] = {9999, 9999, 9999, 9999};
+	//Face at c is bottom
+	if(indexFace == 0) //if face with most white is the top
+	{
+		//Green and blue switch
+		for (int x = 0; x < 4; x++)
+		{
+			tempArray[x] = cube[x+4]; //tempArray equals temp cube at green face 4-7 on index
+			cube[x+4] = cube[x+12];
+			cube[x+12] = tempArray[x];
+		}
+		
+	}
+	for (int x = 0; x < 24; x++)
+	{
+		printf( "Cube at index %d", x, " = %d", cube[x]);
+	}
+	
+}
+
 int main()
 {
 int cube[24];
@@ -374,16 +475,16 @@ cube[0]=5;	//5
 cube[1]=1;	//3
 cube[2]=3;
 cube[3]=5;
-cube[4]=4;
-cube[5]=4;
-cube[6]=1;
-cube[7]=1;
+cube[4]=6;
+cube[5]=6;
+cube[6]=6;
+cube[7]=6;
 cube[8]=2;
 cube[9]=2;
 cube[10]=2;
 cube[11]=2;
-cube[12]=0;
-cube[13]=0;
+cube[12]=3;
+cube[13]=3;
 cube[14]=3;
 cube[15]=3;
 cube[16]=0;
@@ -393,9 +494,21 @@ cube[19]=4;
 cube[20]=3;
 cube[21]=0;
 cube[22]=5;
-cube[23]=5;
+cube[23]=7;
 
 int num = findCase(cube);
 //printf("%d", num);
+
+//CHECK COLOURS WITH ROBSON
+//FACE 1: YELLOW 0-3
+//FACE 2: GREEN 4-7
+//FACE 3: WHITE 8-11
+//FACE 4 : BLUE 12-15
+//FACE 5: RED 16-19
+//FACE 6: ORANGE 20-23 
+
+solvingWhite(cube);
+
+
 return 1;
 }
