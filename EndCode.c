36ba05af9366079e1aa0816f019fdfb13ffacd98
robsonl16 <117456362+robsonl16@users.noreply.github.com>
2022-11-15 21:59:09 -0500

    updated end code

--- EndCode.c
+++ EndCode.c
@@ -27,15 +27,15 @@ void FinishCube(int * cube)
 	{
 		if(cube[22]==cube[23])
 		{
-			if(cube[22]==1) //Green
+			if(cube[22]==1) //Green.
 			{
 				U(CW, cube);
-				rot(CW, cube); //NEED TO UPDATE INDICES
+				rot(CW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
 
-			else if(cube[22]==4) //Red
+			else if(cube[22]==4) //Red.
 			{
 				U(CW, cube);
 				U(CW, cube);
@@ -44,14 +44,14 @@ void FinishCube(int * cube)
 				alg1(cube);
 				U(CW, cube);
 			}
-			else if(cube[22]==3) //Blue
+			else if(cube[22]==3) //Blue.
 			{
 					U(CCW, cube);
 					rot(CCW, cube);
 					alg1(cube);
 					U(CW, cube);
 			}
-			else //Already in right spot
+			else //Already in right spot.
 			{
 					alg1(cube);
 					U(CW, cube);
@@ -59,30 +59,29 @@ void FinishCube(int * cube)
 		}
 		else if(cube[4]==cube[7])
 		{
-			if(cube[4]==4) //Red
+			if(cube[4]==4) //Red.
 			{
-				U(CW, cube);
-				rot(CW, cube);
-				alg1(cube);
-				U(CW, cube);
-			}
-			else if(cube[4]==3) //Blue
-			{
-				U(CW, cube);
 				U(CW, cube);
 				rot(CW, cube);
 				rot(CW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else if(cube[4]==5) //Orange
+			else if(cube[4]==3) //Blue.
+			{
+				U(CW, cube);
+				U(CW, cube);
+				rot(CW, cube);
+				alg1(cube);
+				U(CW, cube);
+			}
+			else if(cube[4]==5) //Orange.
 			{
 				U(CCW, cube);
-				rot(CCW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else //Already in right spot
+			else //Already in right spot.
 			{
 				rot(CCW, cube);
 				alg1(cube);
@@ -93,31 +92,31 @@ void FinishCube(int * cube)
 
 		else if(cube[18]==cube[19])
 		{
-			if(cube[18]==3) //Red
+			if(cube[18]==3) //Blue.
 			{
 				U(CW, cube);
 				rot(CW,cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else if(cube[18]==5) //Orange
+			else if(cube[18]==5) //Orange.
 			{
 				U(CW, cube);
 				U(CW, cube);
-				rot(CW, cube);
-				rot(CW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else if(cube[18]==1) //Green
+			else if(cube[18]==1) //Green.
 			{
 				U(CCW, cube);
 				rot(CCW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else //Already in right spot
+			else //Already in right spot.
 			{
+				rot(CW,cube);
+				rot(CW,cube);
 				alg1(cube);
 				U(CW, cube);
 			}
@@ -127,31 +126,31 @@ void FinishCube(int * cube)
 
 		else
 		{
-			if(cube[13]==5) //Orange
+			if(cube[13]==5) //Orange.
 			{
 				U(CW, cube);
-				rot(CW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else if(cube[13]==1) //Green
+			else if(cube[13]==1) //Green.
 			{
 				U(CW, cube);
 				U(CW, cube);
-				rot(CW, cube);
-				rot(CW, cube);
-				alg1(cube);
-				U(CW, cube);
-			}
-			else if(cube[13]==4) //Red
-			{
-				U(CCW, cube);
 				rot(CCW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
-			else //Already in right spot
+			else if(cube[13]==4) //Red.
 			{
+				U(CCW, cube);
+				rot(CW, cube);
+				rot(CW, cube);
+				alg1(cube);
+				U(CW, cube);
+			}
+			else //Already in right spot Blue .
+			{
+				rot(CW, cube);
 				alg1(cube);
 				U(CW, cube);
 			}
