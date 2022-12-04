0bf111ccdbe0a805c62ad25e229ff9bd7d93359d
TheBarrelPhilospher <108647320+TheBarrelPhilospher@users.noreply.github.com>
2022-12-04 01:18:10 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,18 @@
 #include "FirstOrient.c"
 #include "WhiteSolving.c"
 
-task emergencyStop();
+//imports code from all other files 
+
+/*
+Purpose of file:
+This is the main file. It utilizes code from all the files. This is the file 
+responsible for the code being executed, and is where the cube array is stored.
+
+
+*/
+
+
+task emergencyStop(); //to stop the robot
 
 
 task playMusic()
@@ -44,7 +55,9 @@ task playMusic()
 		}
 	}
 }
-void WriteToFile(int* colour_boundaries_red, int* colour_boundaries_green, int* colour_boundaries_blue)
+
+void WriteToFile(int* colour_boundaries_red, int* colour_boundaries_green, 
+int* colour_boundaries_blue)
 {
 	long colourBoundariesFile = fileOpenWrite("Colour_Boundaries.txt");
 	for(int face = 0; face < SIDES_CUBE; face++)
@@ -56,7 +69,8 @@ void WriteToFile(int* colour_boundaries_red, int* colour_boundaries_green, int* 
 	fileClose(colourBoundariesFile);
 }
 
-void ReadFromFile(int* colour_boundaries_red, int* colour_boundaries_green, int* colour_boundaries_blue)
+void ReadFromFile(int* colour_boundaries_red, int* colour_boundaries_green, 
+int* colour_boundaries_blue)
 {
 	long colourBoundariesFile = fileOpenRead("Colour_Boundaries.txt");
 	for(int face = 0; face < SIDES_CUBE; face++)
@@ -72,52 +86,61 @@ void ReadFromFile(int* colour_boundaries_red, int* colour_boundaries_green, int*
 	fileClose(colourBoundariesFile);
 }
 
-int cube[6*4];
+int cube[6*4]; //creates cube array of size 24
 int cubeCase;
+
+//task responsible for solving cube entirely
 task solveCube()
 {
-cube[0]=2;    //
-cube[1]=5;    //
-cube[2]=0;    //
-cube[3]=0;    //
-cube[4]=2; //
-cube[5]=0;
-cube[6]=2;
-cube[7]=5;    //
-cube[8]=5;
-cube[9]=0;
-cube[10]=3;
-cube[11]=3;
-cube[12]=1;
-cube[13]=1;    //
-cube[14]=4;    //
-cube[15]=2;
-cube[16]=4;
-cube[17]=1;
-cube[18]=1;    //
-cube[19]=4;    //
-cube[20]=5;
-cube[21]=4;
-cube[22]=3;    //
-cube[23]=3;    //
+	//below are test values for the virtual cube
+	cube[0]=2;    //
+	cube[1]=5;    //
+	cube[2]=0;    //
+	cube[3]=0;    //
+	cube[4]=2; //
+	cube[5]=0;
+	cube[6]=2;
+	cube[7]=5;    //
+	cube[8]=5;
+	cube[9]=0;
+	cube[10]=3;
+	cube[11]=3;
+	cube[12]=1;
+	cube[13]=1;    //
+	cube[14]=4;    //
+	cube[15]=2;
+	cube[16]=4;
+	cube[17]=1;
+	cube[18]=1;    //
+	cube[19]=4;    //
+	cube[20]=5;
+	cube[21]=4;
+	cube[22]=3;    //
+	cube[23]=3;    //
 
+	//Calibrating sensors
 	SensorType[S1] = sensorEV3_Color;
 	SensorType[S3] = sensorEV3_Ultrasonic;
 	wait1Msec(50);
-
+	
+	
 	setMotorBrakeMode(motorC, motorBrake);
 	setMotorBrakeMode(motorA, motorBrake);
-
+	
+	//Initialized motorEncoders for each motor
 	nMotorEncoder[motorA] = 0;
 	nMotorEncoder[motorB] = 0;
 	nMotorEncoder[motorC] = 0;
 	nMotorEncoder[motorD] = 0;
+	
+	//Initializing arrays for the RGB testing needed for ScanCube()
 	int colour_boundaries_red[6] = {0,0,0,0,0,0};
 	int colour_boundaries_green[6] = {0,0,0,0,0,0};
 	int colour_boundaries_blue[6] = {0,0,0,0,0,0};
 	const bool CALIBRATE_SENSOR = false;
 
-	if(CALIBRATE_SENSOR)
+	
+	if(CALIBRATE_SENSOR) //if CALIBRATE_SENSOR is true...
 	{
 		while(SensorValue[S3] > 7)
 		{}
@@ -125,15 +148,18 @@ cube[23]=3;    //
 		motor[motorC] = -SENSOR_POWER/2;
 		wait1Msec(500);
 		motor[motorC] = 0;
-		CalibrateColourSensor(colour_boundaries_red, colour_boundaries_green, colour_boundaries_blue);
-		WriteToFile(colour_boundaries_red, colour_boundaries_green, colour_boundaries_blue);
+		CalibrateColourSensor(colour_boundaries_red, colour_boundaries_green, 
+		colour_boundaries_blue);
+		WriteToFile(colour_boundaries_red, colour_boundaries_green, 
+		colour_boundaries_blue);
 		while(SensorValue[S3] < 7)
 		{}
 		wait1Msec(2000);
 	}
 	else
 	{
-		ReadFromFile(colour_boundaries_red, colour_boundaries_green, colour_boundaries_blue);
+		ReadFromFile(colour_boundaries_red, colour_boundaries_green,
+		colour_boundaries_blue);
 	}
 
 	while(SensorValue[S3] > 7)
@@ -148,33 +174,44 @@ cube[23]=3;    //
 
 	//Start timer
 	time1[T1] = 0;
+	
+	//begins scanning cube
+	ScanCube(cube, colour_boundaries_red, colour_boundaries_green, 
+	colour_boundaries_blue);
+	
+	startTask(emergencyStop); //starts the emergency stop
 
-	ScanCube(cube, colour_boundaries_red, colour_boundaries_green, colour_boundaries_blue);
+	solvingWhite(cube);//runs code that orients cube relative to white being 
+	//bottom and solves for the white face
 
-	startTask(emergencyStop);
+	//flips cube twice with indice mappings so that top is now white
+	trackedflip(cube); 
+	trackedflip(cube);
+	
+		
+	FirstOrient(cube); //orients the top row of the cube 
+	cubeCase = findCase(cube); //looks for which case yellow is in
+	orientFace(cube); //solves for the yellow face
+	FinishCube(cube); //solves rest of cube except for last move
 
-	solvingWhite(cube);
-	trackedflip(cube); trackedflip(cube);
-	SetUpCube(cube);
-	FirstOrient(cube);
-	cubeCase = findCase(cube);
-	orientFace(cube);
-	FinishCube(cube);
-
-	displayBigTextLine(5, "%.2f", time1[T1] * MILLI_MINUTES);
+	displayBigTextLine(5, "%.2f", time1[T1] * MILLI_MINUTES); //displays time it
+	//took for the robot to solve the cube
 
 	//End Move
 	hold();
-	startTask(playMusic);
+	startTask(playMusic); //plays still dre
 	wait1Msec(10000);
-	finishingMove(CCW + WHACKER_OFFSET, 1);
-	stopTask(playMusic);
+	finishingMove(CCW + WHACKER_OFFSET, 1); //does the last move with a motor
+	//power of 1
+	stopTask(playMusic); //stops music
 	wait1Msec(1000);
-	playSound(soundBeepBeep);
-	eraseDisplay();
+	playSound(soundBeepBeep); //plays beep beep sound
+	eraseDisplay(); //clears the display
 	stopTask(emergencyStop);
-	done = true;
+	done = true; //bool done is true indicating that the program is done
+	//this exits the loop in task main and the program shuts down.
 }
+
 task emergencyStop()
 {
 	while(!getButtonPress(buttonLeft)){}
@@ -190,8 +227,10 @@ task emergencyStop()
 	motor[FLIPPER_MOTOR] = 0;
 	done = true;
 }
+//main task
 task main()
 {
-	startTask(solveCube);
-	while(!done){}
+	startTask(solveCube); //calls and runs the task responsible for solving the 
+	//cube
+	while(!done){} //waits until it is finished solving
 }
